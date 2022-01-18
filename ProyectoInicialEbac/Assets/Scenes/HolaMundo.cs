10dcfa0e37866b483abdbf93cfdcb8ee2edef145
Gianluca <gianlucadelarosa@gmail.com>
2022-01-18 12:10:30 -0600

    Modificaciones a "Hola Mundo" y se añadió un cubo
    -Se hicieron modificaciones a "Hola Mundo" y se le añadieron funciones como update, awake, entre otras...
    -Se añadió un cubo y 3 scripts como prueba

--- ProyectoInicialEbac/Assets/Scenes/HolaMundo.cs
+++ ProyectoInicialEbac/Assets/Scenes/HolaMundo.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    int x;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("algo pasó");
-        Debug.Log("Algo pasó");
         Debug.LogWarning("Algo salió medianamente mal");
         Debug.LogError("Algo salió muy mal");
     }
@@ -16,6 +17,28 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado"); 
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto ha sido inhabilitado");
     }
 }
