547d04042aed75d378ba1f033885238b0adf7c9d
Gianluca <gianlucadelarosa@gmail.com>
2022-01-22 15:58:39 -0600

    Correciones a la tarea
    -Se añadieron correciones a los scripts, haciendo que usen los prefabs.

--- ProyectoInicialEbac/Assets/Scripts/Cubo2.cs
+++ ProyectoInicialEbac/Assets/Scripts/Cubo2.cs
@@ -4,49 +4,12 @@ using UnityEngine;
 
 public class Cubo2 : MonoBehaviour
 {
-    GameObject objToSpawn;
-    Vector3[] vertices = {
-        new Vector3 (0,0,0), //vertice0
-        new Vector3 (1,0,0), //vertice1
-        new Vector3 (1,1,0), //vertice2
-        new Vector3 (0,1,0), //vertice3
-        new Vector3 (0,1,1), //vertice4
-        new Vector3 (1,1,1), //vertice5
-        new Vector3 (1,0,1), //vertice6
-        new Vector3 (0,0,1), //vertice7
-    };
-
-    int[] triangulos = {
-        0, 2, 1, //Cara 1
-        0, 3, 2,
-        2, 3, 4, //Cara 2
-        2, 4, 5,
-        1, 2, 5, //Cara 3
-        1, 5, 6,
-        0, 7, 4, //Cara 4
-        0, 4, 3,
-        5, 4, 7, //Cara 5
-        5, 7, 6,
-        0, 6, 7, //Cara 6
-        0, 1, 6
-    };
+    public GameObject PrefabCubo;
     public void Awake()
     {
-        objToSpawn = new GameObject("CuboAwake");
-        objToSpawn.AddComponent<MeshFilter>();
-        var meshFilter = objToSpawn.GetComponent<MeshFilter>().mesh;
-        meshFilter.Clear();
-        meshFilter.vertices = vertices;
-        meshFilter.triangles = triangulos;
-        meshFilter.Optimize();
-        meshFilter.RecalculateNormals();
-        objToSpawn.AddComponent<BoxCollider>();
-        var boxCollider = objToSpawn.GetComponent<BoxCollider>();
-        boxCollider.center = new Vector3(0.5f, 0.5f, 0.5f);
-        objToSpawn.AddComponent<MeshRenderer>();
-        var meshRendererMaterial = objToSpawn.GetComponent<MeshRenderer>().material;
-        meshRendererMaterial.color = Color.black;
-        objToSpawn.transform.position = Vector3.one;
+        GameObject tempGameObject = Instantiate<GameObject>(PrefabCubo);
+        tempGameObject.name = "CuboAwake";
+        tempGameObject.transform.position = Random.insideUnitSphere;
     }
     // Start is called before the first frame update
     void Start()
