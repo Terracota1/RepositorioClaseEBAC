39c31affa0497f81cf3f8296655865627fa7f2ff
Gianluca <gianlucadelarosa@gmail.com>
2022-07-08 15:16:12 -0400

    Corrección a la tarea
    Se añadieron funciones split, la conversión de variable float a string ahora muestra 4 dígitos y se terminó de hacer la impresión de caracteres con un índice par de un string

--- ProyectoInicialEbac/Assets/Scripts/Módulo 8/EjerciciosVariablesMod8.cs
+++ ProyectoInicialEbac/Assets/Scripts/Módulo 8/EjerciciosVariablesMod8.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class EjerciciosVariablesMod8 : MonoBehaviour
 {
-    int AumentaValorEnCadaFrame, resultado, modulo, a, b, primerapellido, segundoapellido, inicio1, inicio2, inicio3, final1, final2, final3;
-    int variablemilint, variablemilint2, resultadoTryParse, fraseAcortada;
-    float AumentaValorEnCadaFixedUpdate, resultado2, valor1, valor2;
+    int AumentaValorEnCadaFrame, modulo, a, b, primerapellido, segundoapellido, inicio1, inicio2, inicio3, final1, final2, final3;
+    int variablemilint, variablemilint2, resultadoTryParse, fraseAcortada, posición, posición1;
+    float AumentaValorEnCadaFixedUpdate, valor1, valor2;
     uint Operación;
     string switchcolor, número, númerofinal, nombre, nombreinicial, variabledemil1, variabledemil2, Oración2, Oración;
-    char ParOración;
+    char ParOración, PalabraPar;
 
     enum SelecciónPalabra
     {
@@ -33,22 +33,26 @@ public class EjerciciosVariablesMod8 : MonoBehaviour
         Debug.Log("El resultado de la operación es: "+ Operación);
 
         número = valor2.ToString();
-        númerofinal = número.Substring(0, 5);
+        númerofinal = número.Substring(0, 6);
         Debug.Log("El número recortado es:" + númerofinal);
 
         nombre = "Gianluca de la Rosa Bandini";
         nombreinicial = nombre.Substring(0, 8);
         Debug.Log(nombreinicial);
-
         inicio1 = 9;
         final1 = 19;
         primerapellido = final1 - inicio1;
         Debug.Log(nombre.Substring(inicio1, primerapellido));
-
         inicio2 = 20;
         final2 = 27;
         segundoapellido = final2 - inicio2;
         Debug.Log(nombre.Substring(inicio2, segundoapellido));
+
+        string[] nombreseparado = nombre.Split(' ');
+        foreach (var separación in nombreseparado)
+        {
+            Debug.Log($"Substring: {separación}");
+        }
 
         variabledemil1 = "1056";
         variabledemil2 = "3290";
@@ -57,35 +61,34 @@ public class EjerciciosVariablesMod8 : MonoBehaviour
         resultadoTryParse = (variablemilint + variablemilint2);
         Debug.Log("El resultado de la suma TryParse es: " + resultadoTryParse);
 
+        posición1 = 0;
+        Oración = "Hola Mundo";
+        foreach (char PalabraPar in Oración)
+        {
+            if (posición1 % 2 == 0)
+            {
+                Debug.Log("Letras en par:" + PalabraPar);
+            }
+            posición1++;
+        }
+
+
         Oración2 = "Buenas noches";
         inicio3 = 5;
         final3 = 13;
         fraseAcortada = final3 - inicio3;
         Debug.Log(Oración2.Substring(inicio3, fraseAcortada));
 
-        Oración = "Hola Mundo";
-        ParOración = Oración[1];
-        Debug.Log(ParOración);
-        ParOración = Oración[3];
-        Debug.Log(ParOración);
-        ParOración = Oración[5];
-        Debug.Log(ParOración);
-        ParOración = Oración[7];
-        Debug.Log(ParOración);
-        ParOración = Oración[9];
-        Debug.Log(ParOración);
     }
 
     // Update is called once per frame
     void Update()
     {
-        resultado = AumentaValorEnCadaFrame += 1;
-        Debug.Log(resultado);
+        Debug.Log(AumentaValorEnCadaFrame += 1);
     }
     private void FixedUpdate()
     {
-        resultado2 = AumentaValorEnCadaFixedUpdate *= 1.5f;
-        Debug.Log(resultado2);
+        Debug.Log(AumentaValorEnCadaFixedUpdate *= 1.5f);
 
         modulo = Random.Range(a, b);
         if (modulo % 2 == 0)
